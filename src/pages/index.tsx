5045f6f3492e65edb8f19581d2195a1686d07a54
Ian Marquez <regilmarquez123@gmail.com>
2023-08-06 21:30:17 +0800

    Fix: Fixed headlessui dropdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
+import { type NextPage } from "next";
 import { useSession } from "next-auth/react";
 import { useState } from "react";
 import { InfiniteTweetList } from "~/components/InfiniteTweetList";
-import { LoadingSpinner } from "~/components/LoadingSpinner";
 import { NewTweetForm } from "~/components/NewTweetForm";
 import { api } from "~/utils/api";
 
 const TABS = ["Recent", "Following"] as const;
 
-export default function Home() {
+const Home: NextPage = () => {
   const [selectedTab, setSelectedTab] =
     useState<(typeof TABS)[number]>("Recent");
   const session = useSession();
@@ -17,19 +17,21 @@ export default function Home() {
         <h1 className="mb-2 px-4 text-lg font-bold">Home</h1>
         {session.status === "authenticated" && (
           <div className="flex">
-            {TABS.map((tab) => (
-              <button
-                key={tab}
-                className={`flex-grow p-2 hover:bg-gray-200 focus-visible:bg-gray-200 ${
-                  tab === selectedTab
-                    ? "border-b-4 border-b-blue-500 font-bold"
-                    : ""
-                }`}
-                onClick={() => setSelectedTab(tab)}
-              >
-                {tab}
-              </button>
-            ))}
+            {TABS.map((tab) => {
+              return (
+                <button
+                  key={tab}
+                  className={`flex-grow p-2 hover:bg-gray-200 focus-visible:bg-gray-200 ${
+                    tab === selectedTab
+                      ? "border-b-4 border-b-blue-500 font-bold"
+                      : ""
+                  }`}
+                  onClick={() => setSelectedTab(tab)}
+                >
+                  {tab}
+                </button>
+              );
+            })}
           </div>
         )}
       </header>
@@ -37,13 +39,14 @@ export default function Home() {
       {selectedTab === "Recent" ? <RecentTweets /> : <FollowingTweets />}
     </>
   );
-}
+};
 
 function RecentTweets() {
   const tweets = api.tweet.infiniteFeed.useInfiniteQuery(
     {},
     { getNextPageParam: (lastPage) => lastPage.nextCursor }
   );
+
   return (
     <InfiniteTweetList
       tweets={tweets.data?.pages.flatMap((page) => page.tweets)}
@@ -60,6 +63,7 @@ function FollowingTweets() {
     { onlyFollowing: true },
     { getNextPageParam: (lastPage) => lastPage.nextCursor }
   );
+
   return (
     <InfiniteTweetList
       tweets={tweets.data?.pages.flatMap((page) => page.tweets)}
@@ -70,3 +74,5 @@ function FollowingTweets() {
     />
   );
 }
+
+export default Home;
